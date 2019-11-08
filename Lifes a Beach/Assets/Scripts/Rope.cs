75eab203a1f8d9dba36e3cff59920a54549a18d1
DanielNeale <danielneale01@gmail.com>
2019-11-08 13:58:57 +0000

    Started rope physics - Don't touch

---
+++ Lifes a Beach/Assets/Scripts/Rope.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rope : MonoBehaviour
+{
+    
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
