60bc25e56e9bfc56afc5fa9b39efc3cf035b4198
TobyAtkinson <ta228737@falmouth.ac.uk>
2019-11-10 21:33:29 +0000

    Added test scene with rocks + Child
    Created the first itteration of annoying child and added a new test scene with rocks and navmesh

---
+++ Lifes a Beach/Assets/Scripts/AnnoyingChildScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AnnoyingChildScript : MonoBehaviour
+{
+
+    private GameObject Player;
+
+    NavMeshAgent Enemy;
+
+    private GameObject body;
+
+    private GameObject fallenOver;
+
+
+    void Start()
+    {
+        Enemy = GetComponent<NavMeshAgent>();
+        Player = GameObject.FindGameObjectWithTag("Dog");
+
+        body = this.gameObject.transform.GetChild(0).gameObject;
+      
+        fallenOver = this.gameObject.transform.GetChild(1).gameObject;
+    }
+
+
+    void Update()
+    {
+        Enemy.SetDestination(Player.transform.position);
+    }
+
+    
+
+    IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(1f);
+    }
+
+
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {
+            // player smacks 
+            transform.DetachChildren();
+            fallenOver.SetActive(true);
+            Destroy(body);
+            Destroy(this.gameObject);
+
+        }
+    }
+
+}
+
+
+
