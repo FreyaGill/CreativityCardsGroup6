bddf0707e78651da44ee0eddcdf63a15251466b3
Tosh-Jompson <giblets2k@gmail.com>
2019-11-11 11:36:41 +0000

    Started work on inventory using scriptable objects

---
+++ Lifes a Beach/Assets/Scenes/Ignore this it's not for you/ItemObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemRarity
+{
+    Common,
+    Rare,
+    Epic
+}
+
+public abstract class ItemObject : ScriptableObject
+{
+    public GameObject prefab;
+    public ItemRarity type;
+    [TextArea(15,20)]
+    public string description;
+}
