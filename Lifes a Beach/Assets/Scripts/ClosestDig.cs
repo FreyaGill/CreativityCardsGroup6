78b665ea88b1f14a120023cad125e0ecf26af433
Kalvin Malloch <km230683@falmouth.ac.uk>
2019-11-11 10:47:43 +0000

    Fixed metal detector issue.

---
+++ Lifes a Beach/Assets/Scripts/ClosestDig.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClosestDig : MonoBehaviour
+{
+    public GameObject closestDigObj;
+    public float closestDigDis;
+    public float speed;
+    private Vector3 targetRot;
+
+    void Update()
+    {
+
+        if (closestDigObj != null)
+        {
+
+            closestDigDis = Vector3.Distance(closestDigObj.transform.position, transform.position);
+            Vector3 targetDir = closestDigObj.transform.position - transform.position;
+
+            float step = speed * Time.deltaTime;
+
+        }
+    }
+}
---
+++ Lifes a Beach/Assets/Scripts/MessDistance.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessDistance : MonoBehaviour
+{
+    public float distanceAway;
+    public GameObject playerObj;
+
+    void Start()
+    {
+        playerObj = GameObject.FindWithTag("MetalDetector");
+    }
+
+    void Update()
+    {
+        distanceAway = Vector3.Distance(playerObj.transform.position, transform.position);
+
+        if (playerObj.GetComponent<ClosestDig>().closestDigDis > distanceAway || playerObj.GetComponent<ClosestDig>().closestDigObj == null)
+        {
+            playerObj.GetComponent<ClosestDig>().closestDigObj = this.gameObject;
+
+        }
+    }
+}
