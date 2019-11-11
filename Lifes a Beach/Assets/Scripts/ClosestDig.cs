8ff9834d5453d1720b14a159932a0e8e40126006
Kalvin Malloch <km230683@falmouth.ac.uk>
2019-11-11 11:26:59 +0000

    Implemented the ability for the dog to dig.

--- Lifes a Beach/Assets/Scripts/ClosestDig.cs
+++ Lifes a Beach/Assets/Scripts/ClosestDig.cs
@@ -6,20 +6,14 @@ public class ClosestDig : MonoBehaviour
 {
     public GameObject closestDigObj;
     public float closestDigDis;
-    public float speed;
-    private Vector3 targetRot;
+    public float soundIntensity;
 
     void Update()
     {
 
         if (closestDigObj != null)
         {
-
             closestDigDis = Vector3.Distance(closestDigObj.transform.position, transform.position);
-            Vector3 targetDir = closestDigObj.transform.position - transform.position;
-
-            float step = speed * Time.deltaTime;
-
         }
     }
 }
---
+++ Lifes a Beach/Assets/Scripts/DogDigging.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogDigging : MonoBehaviour
+{
+    void OnTriggerStay(Collider other)
+    {
+        if ((other.gameObject.tag == "DigSpot") &&(Input.GetKey(KeyCode.RightShift)))
+        {
+            print("Got Treasure!");
+            Destroy(other.gameObject);
+        }
+    }
+}
