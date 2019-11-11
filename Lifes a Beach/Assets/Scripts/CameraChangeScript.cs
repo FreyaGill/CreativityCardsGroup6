bde3a3ef7f431bdd3ba6e810532c9ae4b17f119e
Kyle James <kyleajames102@gmail.com>
2019-11-11 14:45:36 +0000

    Worked out camera for main character and began working on camera for shops.

---
+++ Lifes a Beach/Assets/Scripts/CameraChangeScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChangeScript : MonoBehaviour
+{
+    
+    public GameObject vCam;
+
+
+    private void Start()
+    {
+        vCam.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        vCam.SetActive(true);
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        vCam.SetActive(false);
+    }
+}
---
+++ Lifes a Beach/Assets/Scripts/ItemHoverScript.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemHoverScript : MonoBehaviour
+{
+    public GameObject item;
+    public Vector3 startPos;
+    public Vector3 startPos1;
+    public Vector3 endPos;
+    public Vector3 endPos1;
+    public float lerpTime = 5;
+    public float currentLerpTime = 0;
+    public float distance = 30f;
+    public GameObject vCam;
+    private void Start()
+    {
+        vCam.SetActive(false);
+        startPos = item.transform.position;
+        endPos = item.transform.position + Vector3.up * distance;
+        endPos1 = item.transform.position + Vector3.down * distance;
+
+    }
+    private void OnMouseOver()
+    {
+        MoveItemUp();
+    }
+
+
+    private void MoveItemUp()
+    {
+
+        vCam.SetActive(true);
+        currentLerpTime += Time.deltaTime;
+        if (currentLerpTime >= lerpTime)
+        {
+            currentLerpTime = lerpTime;
+        }
+
+        float Perc = currentLerpTime / lerpTime;
+        item.transform.position = Vector3.Lerp(startPos, endPos, Perc);
+
+
+    }
+
+    private void OnMouseExit()
+    {
+        vCam.SetActive(false);
+    }
+
+    /*
+    private void OnMouseExit()
+    {
+        currentLerpTime += Time.deltaTime;
+        
+        if (currentLerpTime >= lerpTime)
+        {
+            currentLerpTime = lerpTime;
+        }
+        
+    
+        float Perc = currentLerpTime / lerpTime;
+
+        item.transform.position = Vector3.Lerp(startPos1, endPos1, Perc);
+
+    }
+    */
+
+}
